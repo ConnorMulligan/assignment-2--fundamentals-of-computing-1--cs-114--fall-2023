040134cd9c5de6c085a06841e551de6918e40eaa
ConnorMulligan <cmulligan@hartford.edu>
2023-10-16 08:38:25 -0400

    Asking for user inputs

--- Diamond.java
+++ Diamond.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
+    int odd_input;
+    int even_input;
+    //Saves user input for later
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a odd number:");
+    odd_input = scan.nextInt();
+    //Saves user input for later
+    System.out.println("Enter a even number");
+    even_input = scan.nextInt();
+
   }
 }
