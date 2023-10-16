2fc3ac9952598f91faf17e181aa6b9b8799b823d
ConnorMulligan <cmulligan@hartford.edu>
2023-10-16 16:52:16 -0400

    Final Code

--- Diamond.java
+++ Diamond.java
@@ -1,16 +1,101 @@
+//Connor Mulligan
 import java.util.Scanner;
+
+import javax.jws.soap.SOAPBinding.Use;
 
 public class Diamond {
   public static void main(String[] args) {
-    int odd_input;
-    int even_input;
+    int UserInput;
+    int length = 1;
+    double diameter;
+
     //Saves user input for later
     Scanner scan = new Scanner(System.in);
-    System.out.println("Enter a odd number:");
-    odd_input = scan.nextInt();
-    //Saves user input for later
-    System.out.println("Enter a even number");
-    even_input = scan.nextInt();
+    System.out.println("Enter a whole number:");
+    UserInput = scan.nextInt();
 
+    diameter = Math.floor(UserInput/2);
+    System.out.println(diameter);
+
+    if(UserInput % 2 != 0){
+      while(length < UserInput){
+        for(int a = 0; a < (UserInput-length)/2; a++){
+          System.out.print(" ");
+        }
+        for(int a = 0; a < length; a++){
+          System.out.print("*");
+        }
+        System.out.println("");
+        length+=2;
+      }
+      if(length == UserInput){
+        for(int a = 0; a < UserInput; a++){
+          System.out.print("*");
+        }
+      }
+      length-=2;
+      System.out.println("");
+      while(length>0){
+        for(int a = 0; a < (UserInput-length)/2; a++){
+          System.out.print(" ");
+        }
+        for(int a = 0; a < length; a++){
+          System.out.print("*");
+        }
+        System.out.println("");
+        length-=2;
+      }
+    }
+    else{
+      length = 1;
+      while(length < 2){
+        for(int a = 0; a < (UserInput-length); a++){
+          System.out.print(" ");
+        }
+        for(int a = 0; a < length; a++){
+          System.out.print("*");
+        }
+        System.out.println("");
+        length = 2;
+      }
+      while(length<UserInput){
+        for (int a = 0; a < (UserInput-length); a++){
+          System.out.print(" ");
+        }
+        for(int a = 0; a<length; a++){
+          System.out.print("*");
+        }
+        System.out.print("");
+        length+=2;
+      }
+      if(length>=UserInput){
+        for(int a = 0; a < UserInput; a++){
+          System.out.print("*");
+        }
+      }
+      length = UserInput-2;
+      System.out.println("");
+      while(length>0){
+        for (int a = 0; a < (UserInput-length); a++){
+          System.out.print(" ");
+        }
+        for(int a = 0; a<length; a++){
+          System.out.print("*");
+        }
+        System.out.println("");
+        length=length-2;
+      }
+      length = 1;
+      while(length < 2){
+        for(int a = 0; a < (UserInput-length); a++){
+          System.out.print(" ");
+        }
+        for(int a = 0; a<length; a++){
+          System.out.print("*");
+        }
+        length=2;
+      }
+    }
+    scan.close();
   }
 }
